f6b0a29232a42cc8b5188b7f7f93bfb27cb9af68
Shreyas <ssk272@cornell.edu>
2015-11-20 02:42:00 -0500

    Refined UI

--- index.php
+++ index.php
@@ -1,2 +1,1 @@
 <? include_once('trueratr.html'); ?>
-
---
+++ search_listing/js/init.js
@@ -0,0 +1,12 @@
+//$(document).ready(function(){
+//    $('.collapsible').collapsible({
+//      accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+//    });
+//  });
+//(function($){
+//  $(function(){
+//
+//    $('.button-collapse').sideNav();
+//
+//  }); // end of document ready
+//})(jQuery); // end of jQuery name space
